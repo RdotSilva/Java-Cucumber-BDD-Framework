3f835004c31b78fe3c3be5840042a5ca19c1c4f8
Ryan Silva <RyandotSilva@gmail.com>
2019-11-18 14:49:23 -0500

    Remove boilerplate code

--- src/main/java/stepDefinitions/ContactUsSteps.java
+++ src/main/java/stepDefinitions/ContactUsSteps.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.DataTable;
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
@@ -35,49 +34,29 @@ public class ContactUsSteps {
 	
 	@Given("^I access webdriveruniversity contact us form$")
 	public void i_access_webdriveruniversity_contact_us_form() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@When("^I enter a valid first name$")
 	public void i_enter_a_valid_first_name() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@When("^I enter a valid last name$")
 	public void i_enter_a_valid_last_name(DataTable arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    // For automatic transformation, change DataTable to one of
-	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
-	    throw new PendingException();
 	}
 
 	@And("^I enter a valid email address$")
 	public void i_enter_a_valid_email_address() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@And("^I enter comments$")
 	public void i_enter_comments(DataTable arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    // For automatic transformation, change DataTable to one of
-	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
-	    throw new PendingException();
 	}
 
 	@When("^I click on the submit button$")
 	public void i_click_on_the_submit_button() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@Then("^the information should successfully be submitted via the contact us form$")
 	public void the_information_should_successfully_be_submitted_via_the_contact_us_form() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 }
--- src/main/java/stepDefinitions/ProductSteps.java
+++ src/main/java/stepDefinitions/ProductSteps.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -32,19 +32,13 @@ public class ProductSteps {
 
 	@Given("^user navigates to \"([^\"]*)\" website$")
 	public void user_navigates_to_website(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@When("^user clicks on \"([^\"]*)\"$")
 	public void user_clicks_on(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@Then("^user should be presented with a promo alert$")
 	public void user_should_be_presented_with_a_promo_alert() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 }
