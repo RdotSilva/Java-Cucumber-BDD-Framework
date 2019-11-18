8b4dfc31e4a71833af75a0c2a29a586712e32cd8
Ryan Silva <RyandotSilva@gmail.com>
2019-11-18 14:13:56 -0500

    Add contact us steps

--- src/main/java/stepDefinitions/ContactUsSteps.java
+++ src/main/java/stepDefinitions/ContactUsSteps.java
@@ -1,5 +1,57 @@
 package stepDefinitions;
 
-public class ContactUsSteps {
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
+public class ContactUsSteps {
+	@Given("^I access webdriveruniversity contact us form$")
+	public void i_access_webdriveruniversity_contact_us_form() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I enter a valid first name$")
+	public void i_enter_a_valid_first_name() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I enter a valid last name$")
+	public void i_enter_a_valid_last_name(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@When("^I enter a valid email address$")
+	public void i_enter_a_valid_email_address() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I enter comments$")
+	public void i_enter_comments(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@When("^I click on the submit button$")
+	public void i_click_on_the_submit_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the information should successfully be submitted via the contact us form$")
+	public void the_information_should_successfully_be_submitted_via_the_contact_us_form() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 }
--- src/test/java/resources/features/ContactUs.feature
+++ src/test/java/resources/features/ContactUs.feature
@@ -4,7 +4,7 @@ Scenario: Submit valid data via contact us form
 	Given I access webdriveruniversity contact us form
 	When I enter a valid first name
 	When I enter a valid last name
-	| woods | jackson | jones
+	| woods | jackson | jones |
 	And I enter a valid email address
 	And I enter comments
 	| example comment one | example comment two |
