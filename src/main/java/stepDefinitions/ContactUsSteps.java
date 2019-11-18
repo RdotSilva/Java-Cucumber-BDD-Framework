9df1935d6df0387b5970a120d7154aaf48257158
Ryan Silva <RyandotSilva@gmail.com>
2019-11-18 15:11:01 -0500

    Update contact us steps

--- src/main/java/stepDefinitions/ContactUsSteps.java
+++ src/main/java/stepDefinitions/ContactUsSteps.java
@@ -2,9 +2,12 @@ package stepDefinitions;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.DataTable;
@@ -14,6 +17,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import junit.framework.Assert;
 
 public class ContactUsSteps {
 	WebDriver driver;
@@ -33,30 +37,45 @@ public class ContactUsSteps {
 	}
 	
 	@Given("^I access webdriveruniversity contact us form$")
-	public void i_access_webdriveruniversity_contact_us_form() throws Throwable {
+	public void i_access_webdriveruniversity_contact_us_form() {
+		driver.get("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
 	}
 
 	@When("^I enter a valid first name$")
-	public void i_enter_a_valid_first_name() throws Throwable {
+	public void i_enter_a_valid_first_name() {
+		driver.findElement(By.cssSelector("input[name='first_name']")).sendKeys("tom");
 	}
 
 	@When("^I enter a valid last name$")
-	public void i_enter_a_valid_last_name(DataTable arg1) throws Throwable {
+	public void i_enter_a_valid_last_name(DataTable dataTable) {
+		List<List<String>> data = dataTable.raw();
+		
+		driver.findElement(By.cssSelector("input[name='last_name']")).sendKeys(data.get(0).get(1));
 	}
 
 	@And("^I enter a valid email address$")
-	public void i_enter_a_valid_email_address() throws Throwable {
+	public void i_enter_a_valid_email_address() {
+		driver.findElement(By.cssSelector("input[name='email']")).sendKeys("fake@email.com");
 	}
 
 	@And("^I enter comments$")
-	public void i_enter_comments(DataTable arg1) throws Throwable {
+	public void i_enter_comments(DataTable dataTable) {
+		List<List<String>> data = dataTable.raw();
+	
+		driver.findElement(By.cssSelector("textarea[name='message']")).sendKeys(data.get(0).get(0));
+		driver.findElement(By.cssSelector("textarea[name='message']")).sendKeys(data.get(0).get(1));
 	}
 
 	@When("^I click on the submit button$")
-	public void i_click_on_the_submit_button() throws Throwable {
+	public void i_click_on_the_submit_button() {
+		driver.findElement(By.xpath("//input[@value='SUBMIT']")).click();
 	}
 
 	@Then("^the information should successfully be submitted via the contact us form$")
-	public void the_information_should_successfully_be_submitted_via_the_contact_us_form() throws Throwable {
+	public void the_information_should_successfully_be_submitted_via_the_contact_us_form() {
+
+		WebElement thanksContactUsPage = driver.findElement(By.xpath("//*[@id='contact_reply']/h1"));
+		
+		Assert.assertEquals("thankyouforyourmessage!", thanksContactUsPage.getText().toLowerCase().replaceAll("[ ()0-9]", ""));
 	}
 }
