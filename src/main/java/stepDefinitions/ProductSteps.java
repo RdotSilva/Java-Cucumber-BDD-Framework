edf246669f12efd89a697bd23e0b546f2e2aabe1
Ryan Silva <RyandotSilva@gmail.com>
2019-11-18 14:25:59 -0500

    Add Before hook

--- src/main/java/stepDefinitions/ProductSteps.java
+++ src/main/java/stepDefinitions/ProductSteps.java
@@ -1,11 +1,28 @@
 package stepDefinitions;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import cucumber.api.PendingException;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class ProductSteps {
+	WebDriver driver;
+	
+	@Before()
+	public void setup() throws IOException {
+		System.setProperty("webdriver.chrome.driver", Paths.get(System.getProperty("user.dir")).toRealPath() + "\\src\\test\\java\\resources\\other\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.driver.manage().window().maximize();
+		this.driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
+	}
 
 	@Given("^user navigates to \"([^\"]*)\" website$")
 	public void user_navigates_to_website(String arg1) throws Throwable {
