4fd7907a4c5c20c23dd73191c72f9fac00de44b2
Ryan Silva <RyandotSilva@gmail.com>
2019-11-18 13:40:12 -0500

    Initial commit

---
+++ src/test/java/runners/MainRunner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features = { "src/test/java/resources/features/" }, glue = {
+		"stepDefinitions" }, monochrome = true, tags = {}, 
+				plugin = { "pretty", "html:target/cucumber", "json:target/cucumber.json",
+				"com.cucumber.listener.ExtentCucumberFormatter:output/report.html" })
+
+public class MainRunner extends AbstractTestNGCucumberTests {
+
+}
