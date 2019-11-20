546cad579356b5b7f72b381fb7b8fecd7e60833f
Ryan Silva <RyandotSilva@gmail.com>
2019-11-19 21:09:00 -0500

    Add Constant class

---
+++ src/main/java/utils/Constant.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Constant {
+	public final static String CONFIG_PROPERTIES_DIRECTORY = "";
+	public final static String GECKO_PROPERTIES_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\other\\geckodriver.exe";
+	public final static String CHROME_PROPERTIES_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\other\\chromedriver.exe";
+	public final static String IE_PROPERTIES_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\resources\\other\\IEDriverServer.exe";
+}
