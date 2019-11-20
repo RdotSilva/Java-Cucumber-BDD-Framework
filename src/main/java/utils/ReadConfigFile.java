b48295cb1f1aab01d78caca99a31ef62337b388c
Ryan Silva <RyandotSilva@gmail.com>
2019-11-19 21:13:31 -0500

    Add ReadConfigFile class

---
+++ src/main/java/utils/ReadConfigFile.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadConfigFile {
+	protected InputStream input = null;
+	protected Properties prop = null;
+
+}
