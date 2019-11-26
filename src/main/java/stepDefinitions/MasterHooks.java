cf3f2cce08bf51fea0641546e82a01c00ba11b7e
Ryan Silva <RyandotSilva@gmail.com>
2019-11-26 10:43:21 -0500

    Add MasterHooks class

---
+++ src/main/java/stepDefinitions/MasterHooks.java
@@ -0,0 +1,21 @@
+package stepDefinitions;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import utils.DriverFactory;
+
+public class MasterHooks extends DriverFactory{
+	
+	@Before
+	public void setup() throws Exception {
+		driver = getDriver();
+	}
+	
+	@After
+	public void tearDown() {
+		if(driver != null) {
+			driver.manage().deleteAllCookies();
+			driver.quit();
+		}
+	}
+}
