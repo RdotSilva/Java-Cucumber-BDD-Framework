76a6e67832c49fcea198bd4021e24f21b9007e4c
Ryan Silva <RyandotSilva@gmail.com>
2019-11-26 10:53:37 -0500

    Update ProductSteps to use DriverFactory

--- src/main/java/stepDefinitions/ProductSteps.java
+++ src/main/java/stepDefinitions/ProductSteps.java
@@ -1,49 +1,26 @@
 package stepDefinitions;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import utils.DriverFactory;
 
-public class ProductSteps {
-	WebDriver driver;
-	
-	@Before()
-	public void setup() throws IOException {
-		System.setProperty("webdriver.chrome.driver", Paths.get(System.getProperty("user.dir")).toRealPath() + "\\src\\test\\java\\resources\\other\\chromedriver.exe");
-		this.driver = new ChromeDriver();
-		this.driver.manage().window().maximize();
-		this.driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-	}
-	
-	@After()
-	public void tearDown() {
-		this.driver.manage().deleteAllCookies();
-		this.driver.quit();
-	}
-
+public class ProductSteps extends DriverFactory {
 	@Given("^user navigates to \"([^\"]*)\" website$")
-	public void user_navigates_to_website(String url) {
-		driver.get(url);
+	public void user_navigates_to_website(String url) throws Exception {
+		getDriver().get(url);
 	}
 
 	@When("^user clicks on \"([^\"]*)\"$")
-	public void user_clicks_on(String locator) {
-		driver.findElement(By.cssSelector(locator)).click();
+	public void user_clicks_on(String locator) throws Exception {
+		getDriver().findElement(By.cssSelector(locator)).click();
 	}
 
 	@Then("^user should be presented with a promo alert$")
-	public void user_should_be_presented_with_a_promo_alert() throws InterruptedException {
+	public void user_should_be_presented_with_a_promo_alert() throws Exception {
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//button[text()='Proceed']")).click();
+		getDriver().findElement(By.xpath("//button[text()='Proceed']")).click();
 	}
 }
