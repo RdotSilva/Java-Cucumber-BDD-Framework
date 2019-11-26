32413f3edae2597b6691c6a9b250c77384a99d68
Ryan Silva <RyandotSilva@gmail.com>
2019-11-26 10:52:03 -0500

    Update ContactUsSteps to use DriverFactory

--- src/main/java/stepDefinitions/ContactUsSteps.java
+++ src/main/java/stepDefinitions/ContactUsSteps.java
@@ -1,80 +1,59 @@
 package stepDefinitions;
 
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.DataTable;
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import junit.framework.Assert;
+import utils.DriverFactory;
 
-public class ContactUsSteps {
-	WebDriver driver;
-	
-	@Before()
-	public void setup() throws IOException {
-		System.setProperty("webdriver.chrome.driver", Paths.get(System.getProperty("user.dir")).toRealPath() + "\\src\\test\\java\\resources\\other\\chromedriver.exe");
-		this.driver = new ChromeDriver();
-		this.driver.manage().window().maximize();
-		this.driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-	}
-	
-	@After()
-	public void tearDown() {
-		this.driver.manage().deleteAllCookies();
-		this.driver.quit();
-	}
+public class ContactUsSteps extends DriverFactory {
 	
 	@Given("^I access webdriveruniversity contact us form$")
-	public void i_access_webdriveruniversity_contact_us_form() {
-		driver.get("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+	public void i_access_webdriveruniversity_contact_us_form() throws Exception {
+		getDriver().get("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
 	}
 
 	@When("^I enter a valid first name$")
-	public void i_enter_a_valid_first_name() {
-		driver.findElement(By.cssSelector("input[name='first_name']")).sendKeys("tom");
+	public void i_enter_a_valid_first_name() throws Exception {
+		getDriver().findElement(By.cssSelector("input[name='first_name']")).sendKeys("tom");
 	}
 
 	@When("^I enter a valid last name$")
-	public void i_enter_a_valid_last_name(DataTable dataTable) {
+	public void i_enter_a_valid_last_name(DataTable dataTable) throws Exception {
 		List<List<String>> data = dataTable.raw();
 		
-		driver.findElement(By.cssSelector("input[name='last_name']")).sendKeys(data.get(0).get(1));
+		getDriver().findElement(By.cssSelector("input[name='last_name']")).sendKeys(data.get(0).get(1));
 	}
 
 	@And("^I enter a valid email address$")
-	public void i_enter_a_valid_email_address() {
-		driver.findElement(By.cssSelector("input[name='email']")).sendKeys("fake@email.com");
+	public void i_enter_a_valid_email_address() throws Exception {
+		getDriver().findElement(By.cssSelector("input[name='email']")).sendKeys("fake@email.com");
 	}
 
 	@And("^I enter comments$")
-	public void i_enter_comments(DataTable dataTable) {
+	public void i_enter_comments(DataTable dataTable) throws Exception {
 		List<List<String>> data = dataTable.raw();
 	
-		driver.findElement(By.cssSelector("textarea[name='message']")).sendKeys(data.get(0).get(0));
-		driver.findElement(By.cssSelector("textarea[name='message']")).sendKeys(data.get(0).get(1));
+		getDriver().findElement(By.cssSelector("textarea[name='message']")).sendKeys(data.get(0).get(0));
+		getDriver().findElement(By.cssSelector("textarea[name='message']")).sendKeys(data.get(0).get(1));
 	}
 
 	@When("^I click on the submit button$")
-	public void i_click_on_the_submit_button() {
-		driver.findElement(By.xpath("//input[@value='SUBMIT']")).click();
+	public void i_click_on_the_submit_button() throws Exception {
+		getDriver().findElement(By.xpath("//input[@value='SUBMIT']")).click();
 	}
 
 	@Then("^the information should successfully be submitted via the contact us form$")
-	public void the_information_should_successfully_be_submitted_via_the_contact_us_form() {
+	public void the_information_should_successfully_be_submitted_via_the_contact_us_form() throws Exception {
 
-		WebElement thanksContactUsPage = driver.findElement(By.xpath("//*[@id='contact_reply']/h1"));
+		WebElement thanksContactUsPage = getDriver().findElement(By.xpath("//*[@id='contact_reply']/h1"));
 		
 		Assert.assertEquals("thankyouforyourmessage!", thanksContactUsPage.getText().toLowerCase().replaceAll("[ ()0-9]", ""));
 	}
