2eabc935c7ff4b6a995fb66f4436b9ae133be008
Ryan Silva <RyandotSilva@gmail.com>
2019-11-26 11:27:20 -0500

    Add missing input variable

--- src/main/java/utils/ReadConfigFile.java
+++ src/main/java/utils/ReadConfigFile.java
@@ -10,17 +10,18 @@ public class ReadConfigFile {
 
 	public ReadConfigFile() {
 		try {
-			ReadConfigFile.class.getClassLoader().getResourceAsStream(Constant.CONFIG_PROPERTIES_DIRECTORY);
+			input = ReadConfigFile.class.getClassLoader().getResourceAsStream(Constant.CONFIG_PROPERTIES_DIRECTORY);
 			prop = new Properties();
 			prop.load(input);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public String getBrowser() {
 		if (prop.getProperty("browser") == null)
 			return "";
 		return prop.getProperty("browser");
 	}
+	
 }
