42f13c70fa84875c6416f0ff77004642205d168f
Ryan Silva <RyandotSilva@gmail.com>
2019-11-26 15:09:10 -0500

    Add BasePage init to DriverFactory

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 
+import pageObjects.BasePage;
 import pageObjects.ContactUs_Page;
 import pageObjects.Products_Page;
 
@@ -17,6 +18,7 @@ public class DriverFactory {
 	public static WebDriver driver;
 	public static ContactUs_Page contactUsPage;
 	public static Products_Page productsPage;
+	public static BasePage basePage;
 
 	public WebDriver getDriver() throws Exception {
 		try {
@@ -63,6 +65,7 @@ public class DriverFactory {
 			// Initialize pages
 			contactUsPage = PageFactory.initElements(driver, ContactUs_Page.class);
 			productsPage = PageFactory.initElements(driver, Products_Page.class);
+			basePage = PageFactory.initElements(driver, BasePage.class);
 		}
 		return driver;
 	}
