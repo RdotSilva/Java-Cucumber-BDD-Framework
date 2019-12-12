af41e8ca24515e4181458adc4e228c1b36e9a642
RdotSilva <RyandotSilva@gmail.com>
2019-12-11 19:44:32 -0500

    Add slider logic to BmiCalculatorPage

--- src/main/java/pageObjects/BmiCalculatorPage.java
+++ src/main/java/pageObjects/BmiCalculatorPage.java
@@ -4,51 +4,59 @@ import java.io.IOException;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class BmiCalculatorPage extends BasePage{
-	public @FindBy(xpath = "//div[@id='slider_weight']//a[@class='ui-slider-handle']") WebElement weightSlider;
-	public @FindBy(xpath = "//div[@id='slider_height_feet']//a[@class='ui-slider-handle']]") WebElement feetSlider;
-	public @FindBy(xpath = "//div[@id='slider_height_inch']//a[@class='ui-slider-handle']") WebElement inchesSlider;
+	public @FindBy(xpath = "//div[@id='slider_weight']//a") WebElement weightSlider;
+	public @FindBy(xpath = "//div[@id='slider_height_feet']//a") WebElement feetSlider;
+	public @FindBy(xpath = "//div[@id='slider_height_inch']//a") WebElement inchesSlider;
 	
 	public BmiCalculatorPage() throws IOException {
 		super();
 	}
 	
-	public BmiCalculatorPage getContactUsPage() throws Exception {
+	public BmiCalculatorPage getBmiCalculatorPage() throws Exception {
 		getDriver().get("https://www.acefitness.org/education-and-resources/lifestyle/tools-calculators/bmi-calculator");
 		return new BmiCalculatorPage();
 	}
 	
-	public BmiCalculatorPage enterWeight() throws Exception {
+	public BmiCalculatorPage enterWeight(int weight) throws Exception {
+		int numberOfPresses = weight - 145;
+		
+		for (int i = 0; i < numberOfPresses ; i++)
+		{
+			weightSlider.sendKeys(Keys.ARROW_RIGHT);
+		}
+		return new BmiCalculatorPage();
+	}
+	
+	public BmiCalculatorPage enterHeightFeet(int feet) throws Exception {
 		Actions move = new Actions(driver);
-		Action action = (Action) move.dragAndDropBy(weightSlider, 30, 0).build();
+		Action action = (Action) move.dragAndDropBy(feetSlider, offSetValue, 0).build();
 		action.perform();
 		return new BmiCalculatorPage();
 	}
 	
-	public BmiCalculatorPage enterHeightFeet() throws Exception {
+	public BmiCalculatorPage enterHeightInches() throws Exception {
+		int offSetValuePerInch = 20;
+		
 		Actions move = new Actions(driver);
-		Action action = (Action) move.dragAndDropBy(feetSlider, 30, 0).build();
-		action.perform();
-		return new BmiCalculatorPage();
-	}
-	
-	public BmiCalculatorPage enterHeightInches(String inches) throws Exception {
-		Actions move = new Actions(driver);
-		Action action = (Action) move.dragAndDropBy(inchesSlider, 30, 0).build();
+		Action action = (Action) move.dragAndDropBy(inchesSlider, offSetValuePerInch * 2, 0).build();
 		action.perform();
 		return new BmiCalculatorPage();
 	}
 	
 	public BmiCalculatorPage confirmCorrectBMI() throws Exception {
+		String expectedDisplayedBMI = "16.636";
 		
-		WebElement expectedBMI = getDriver().findElement(By.xpath("//input[@id='amount_bmi']"));
-		WaitUntilWebElementIsVisible(expectedBMI);
-		Assert.assertEquals(22.76, expectedBMI.getAttribute("value"));
+		WebElement displayedBMI = getDriver().findElement(By.xpath("//input[@id='amount_bmi']"));
+		WaitUntilWebElementIsVisible(displayedBMI);
+		
+		Assert.assertEquals(expectedDisplayedBMI, displayedBMI.getAttribute("value"));
 		return new BmiCalculatorPage();
 	}
 
