398078385879f68a5306f2c634ac26934ff47a6f
RdotSilva <RyandotSilva@gmail.com>
2019-12-12 15:35:24 -0500

    Update steps to use BMI calc page methods

--- src/main/java/stepDefinitions/BmiCalculatorSteps.java
+++ src/main/java/stepDefinitions/BmiCalculatorSteps.java
@@ -1,43 +1,36 @@
 package stepDefinitions;
 
 import cucumber.api.DataTable;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import utils.DriverFactory;
 
-public class BmiCalculatorSteps {
+public class BmiCalculatorSteps extends DriverFactory{
+	
 	@Given("^I access acefitness BMI calculator$")
-	public void i_access_acefitness_BMI_calculator() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void i_access_acefitness_BMI_calculator() throws Exception {
+		bmiCalculatorPage.getBmiCalculatorPage();
 	}
 
 	@When("^I enter a valid weight$")
-	public void i_enter_a_valid_weight() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void i_enter_a_valid_weight() throws Exception {
+	    bmiCalculatorPage.enterWeight(155);
 	}
 
 	@When("^I enter a valid height in feet$")
-	public void i_enter_a_valid_height_in_feet(DataTable arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    // For automatic transformation, change DataTable to one of
-	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
-	    throw new PendingException();
+	public void i_enter_a_valid_height_in_feet() throws Exception {
+	    bmiCalculatorPage.enterHeightFeet(6);
 	}
 
 	@When("^I enter a valid height in inches$")
-	public void i_enter_a_valid_height_in_inches() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void i_enter_a_valid_height_in_inches(DataTable inchDataTable) throws Exception {
+	    bmiCalculatorPage.enterHeightInches(inchDataTable, 0, 8);
 	}
 
 	@Then("^The estimated BMI value should be displayed correctly$")
-	public void the_estimated_BMI_value_should_be_displayed_correctly() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void the_estimated_BMI_value_should_be_displayed_correctly() throws Exception {
+	    bmiCalculatorPage.confirmCorrectBMI();
 	}
 
 }
