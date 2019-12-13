f34c958fc710e91470353f3aebef5f79fa36d699
RdotSilva <RyandotSilva@gmail.com>
2019-12-13 17:10:30 -0500

    Add WebElements for login

--- src/main/java/pageObjects/MealPlannerPage.java
+++ src/main/java/pageObjects/MealPlannerPage.java
@@ -1,5 +1,24 @@
 package pageObjects;
 
-public class MealPlannerPage {
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class MealPlannerPage extends BasePage {
+
+	public @FindBy(xpath = "//a[@class='sign-in-link']") WebElement loginLink;
+	public @FindBy(xpath = "//input[@placeholder='Email or Username']") WebElement usernameInput;
+	public @FindBy(xpath = "//input[@placeholder='Password']") WebElement passwordInput;
 	
+	public MealPlannerPage() throws IOException {
+		super();
+	}
+		
 }
