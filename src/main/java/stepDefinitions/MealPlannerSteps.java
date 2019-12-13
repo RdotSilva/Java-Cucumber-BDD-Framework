5f2d92f9de3bc8f03f876aaead95e1bbadaf1e5d
RdotSilva <RyandotSilva@gmail.com>
2019-12-13 11:10:36 -0500

    Add generated step snippets

---
+++ src/main/java/stepDefinitions/MealPlannerSteps.java
@@ -0,0 +1,58 @@
+package stepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import utils.DriverFactory;
+
+public class MealPlannerSteps extends DriverFactory{
+	@Given("^user logged in as a registered user$")
+	public void user_logged_in_as_a_registered_user() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user navigates to the eatthismuch\\.com website$")
+	public void user_navigates_to_the_eatthismuch_com_website() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user navigates to the login page$")
+	public void user_navigates_to_the_login_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user enters a valid username$")
+	public void user_enters_a_valid_username() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user enters a valid password$")
+	public void user_enters_a_valid_password() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user should be presented with the Meal Planner page$")
+	public void user_should_be_presented_with_the_Meal_Planner_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^user is not logged in as a registered user$")
+	public void user_is_not_logged_in_as_a_registered_user() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user should be presented with the landing page$")
+	public void user_should_be_presented_with_the_landing_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
