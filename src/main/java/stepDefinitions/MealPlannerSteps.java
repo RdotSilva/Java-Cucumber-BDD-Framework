1e6fca7c78a6972cc671b1daf069935ca9561bb8
RdotSilva <RyandotSilva@gmail.com>
2019-12-14 21:49:15 -0500

    Update MealPlannerSteps

--- src/main/java/stepDefinitions/MealPlannerSteps.java
+++ src/main/java/stepDefinitions/MealPlannerSteps.java
@@ -8,39 +8,33 @@ import utils.DriverFactory;
 
 public class MealPlannerSteps extends DriverFactory{
 	@Given("^user logged in as a registered user$")
-	public void user_logged_in_as_a_registered_user() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_logged_in_as_a_registered_user() throws Exception {
+	    
 	}
 
 	@When("^user navigates to the eatthismuch\\.com website$")
-	public void user_navigates_to_the_eatthismuch_com_website() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_navigates_to_the_eatthismuch_com_website() throws Exception {
+	    mealPlannerPage.getMealPlannerPage();
 	}
 
 	@When("^user navigates to the login page$")
-	public void user_navigates_to_the_login_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_navigates_to_the_login_page() throws Exception {
+	    mealPlannerPage.clickLoginLink();
 	}
 
 	@When("^user enters a valid username$")
-	public void user_enters_a_valid_username() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_enters_a_valid_username() throws Exception {
+	    mealPlannerPage.enterUsername("mealplan999");
 	}
 
 	@When("^user enters a valid password$")
-	public void user_enters_a_valid_password() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_enters_a_valid_password() throws Exception {
+	    mealPlannerPage.enterPassword("4FB&2c$");
 	}
 
 	@Then("^user should be presented with the Meal Planner page$")
-	public void user_should_be_presented_with_the_Meal_Planner_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_should_be_presented_with_the_Meal_Planner_page() throws Exception {
+	    mealPlannerPage.confirmUserCanAccessMealPlanner();
 	}
 
 	@Given("^user is not logged in as a registered user$")
