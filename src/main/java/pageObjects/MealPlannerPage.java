2e143fa9ac40d9fdca76992ccdf401bfa06901d2
RdotSilva <RyandotSilva@gmail.com>
2019-12-15 22:05:11 -0500

    Update page w/ working meal planner access

--- src/main/java/pageObjects/MealPlannerPage.java
+++ src/main/java/pageObjects/MealPlannerPage.java
@@ -16,6 +16,7 @@ public class MealPlannerPage extends BasePage {
 	public @FindBy(xpath = "//a[@class='sign-in-link']") WebElement loginLink;
 	public @FindBy(xpath = "//input[@placeholder='Email or Username']") WebElement usernameInput;
 	public @FindBy(xpath = "//input[@placeholder='Password']") WebElement passwordInput;
+	public @FindBy(xpath = "//button[contains(text(), 'Edit Week')]") WebElement mealPlannerConfirmText;
 	
 	public MealPlannerPage() throws IOException {
 		super();
@@ -38,20 +39,26 @@ public class MealPlannerPage extends BasePage {
 	
 	public MealPlannerPage enterPassword(String password) throws Exception {
 		sendKeysToWebElement(passwordInput, password);
+		
+		return new MealPlannerPage();
+	}
+	
+	public MealPlannerPage clickOnSubmitButton() throws Exception {
+		WebElement submitButton = getDriver().findElement(By.xpath("//button[contains(text(), 'Log in')]"));
+		waitAndClickElement(submitButton);
 		return new MealPlannerPage();
 	}
 	
 	public MealPlannerPage confirmUserCanAccessMealPlanner() throws Exception {
-		WebElement mealPlannerConfirmText = getDriver().findElement(By.xpath("//div[@class='carousel_header_text']"));
 		WaitUntilWebElementIsVisible(mealPlannerConfirmText);
-		Assert.assertEquals("Meal Planner", mealPlannerConfirmText.getText());
+		Assert.assertEquals("Edit Week", mealPlannerConfirmText.getText());
 		return new MealPlannerPage();
 	}
 	
 	public MealPlannerPage confirmUserOnLandingPage() throws Exception {
 		WebElement landingPageConfirmText = getDriver().findElement(By.xpath("//h1[contains(text(),'Put your diet on autopilot')]"));
 		WaitUntilWebElementIsVisible(landingPageConfirmText);
-		Assert.assertEquals("Put yout diet on autopilot", landingPageConfirmText.getText());
+		Assert.assertEquals("Put your diet on autopilot", landingPageConfirmText.getText());
 		return new MealPlannerPage();
 	}
 		
