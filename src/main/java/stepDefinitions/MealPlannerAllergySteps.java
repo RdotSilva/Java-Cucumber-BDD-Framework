42b79b4ae4105a7c9580fe64578a5e9e0b09ab2f
RdotSilva <RyandotSilva@gmail.com>
2019-12-19 16:36:30 -0500

    Add MealPlannerAllergySteps file

---
+++ src/main/java/stepDefinitions/MealPlannerAllergySteps.java
@@ -0,0 +1,34 @@
+package stepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import utils.DriverFactory;
+
+public class MealPlannerAllergySteps extends DriverFactory {
+	
+	@Given("^user has selected common allergies from the Diet Exclusions setting$")
+	public void user_has_selected_common_allergies_from_the_Diet_Exclusions_setting() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user navigates to the meal planner page$")
+	public void user_navigates_to_the_meal_planner_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user clicks the meal generation button$")
+	public void user_clicks_the_meal_generation_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the meal planner should not show any meals with allergies$")
+	public void the_meal_planner_should_not_show_any_meals_with_allergies() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
