49ea09a63df39dbc05247e342509cae17d84f339
RdotSilva <RyandotSilva@gmail.com>
2019-12-26 12:17:52 -0500

    Update steps with method calls

--- src/main/java/stepDefinitions/MealPlannerAllergySteps.java
+++ src/main/java/stepDefinitions/MealPlannerAllergySteps.java
@@ -9,26 +9,22 @@ import utils.DriverFactory;
 public class MealPlannerAllergySteps extends DriverFactory {
 	
 	@Given("^user has selected common allergies from the Diet Exclusions setting$")
-	public void user_has_selected_common_allergies_from_the_Diet_Exclusions_setting() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_has_selected_common_allergies_from_the_Diet_Exclusions_setting() throws Exception {
+	    mealPlannerPage.checkForSelectedAllergies();
 	}
 
 	@When("^user navigates to the meal planner page$")
-	public void user_navigates_to_the_meal_planner_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_navigates_to_the_meal_planner_page() throws Exception {
+		mealPlannerPage.getMealPlannerPage();
 	}
 
 	@When("^user clicks the meal generation button$")
-	public void user_clicks_the_meal_generation_button() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_clicks_the_meal_generation_button() throws Exception {
+	    mealPlannerPage.clickOnGenerateMealButton();
 	}
 
 	@Then("^the meal planner should not show any meals with allergies$")
-	public void the_meal_planner_should_not_show_any_meals_with_allergies() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void the_meal_planner_should_not_show_any_meals_with_allergies() throws Exception {
+	    mealPlannerPage.checkMealsForAllergies();
 	}
 }
